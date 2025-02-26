6c2afaf8019a6079b4dafb9f928cc008dcf83ae0
Indk0 <inderik302@gmail.com>
2025-02-26 11:11:55 +0000

    Add transfers code

--- bank_accounts.py
+++ bank_accounts.py
@@ -19,9 +19,26 @@ class BankAccount:
     
     def payments_transfers(self):
         if BankAccount.deposit():
-            self.balance += deposit()
+            deposit = input(float("Enter amount to be deposited:"))
+            self.balance += deposit
+            print("Amount deposited:", deposit)
+            print("Congrations on defending againts the Vikings!!")
+        
+        elif BankAccount.withdraw(self):
+            withdraw = input(float("Enter amount to be withdrawn:"))
+            if self.balance >= withdraw:
+                self.balance -= withdraw
+                print("You withdrew:", withdraw)
+                print("Conquering more land? Good luck!")
+            else:
+                print("Insufficient balance.")
+
+        def display(self):
+            print(f"Your remaining balance:", self.balance)
 
 
 
 Leofric = BankAccount('Leofric', 'Earl of Mercia', '96810571043', 'Current Account', '1040', '£3,097,576,293')
 
+Leofric.display_wel_mes
+Leofric.home
